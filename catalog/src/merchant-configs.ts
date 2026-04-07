0f8060884a5617907c174cee00119fabbde70bc6
Omer Akkentli <omer@glov.ai>
2026-04-07 15:26:07 +0300

    chore: remove unintended merchant-config update

--- catalog/src/merchant-configs.ts
+++ catalog/src/merchant-configs.ts
@@ -62,7 +62,7 @@ const MERCHANT_CONFIGS: Record<string, MerchantConfig> = {
     },
     chatI18n: {
       inputPlaceholder: 'Ne aramıştınız?',
-      poweredBy: 'Gengage',
+      poweredBy: 'n11 AI Asistan',
     },
   },
 
